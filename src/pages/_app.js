f49570ec1d18c2ef5f4439f9fd4e5d34254effd2
arefin <arefinnuman2000@gmail.com>
2023-04-11 12:18:10 +0600

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Footer from "./layouts/footer";
+import Header from "./layouts/header";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <div className="max-w-[1440px] mx-auto">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#7049e5",
+
+          secondary: "#9ebae5",
+
+          accent: "#a0084a",
+
+          neutral: "#201D25",
+
+          "base-100": "#EFEFF5",
+
+          info: "#9ED5E0",
+
+          success: "#1A9E6D",
+
+          warning: "#FBA141",
+
+          error: "#F15B5B",
+        },
+      },
+    ],
+  },
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+};
